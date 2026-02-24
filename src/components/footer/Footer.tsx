7fa72161ce178947ad889696d0c10fb98e14475c
jujubeguud <jpak1203@gmail.com>
2026-02-24 17:27:42 -0500

    enhance footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,7 @@
-import { Flex, Link, Group, Input, Button, Field } from '@chakra-ui/react';
+import { Flex, Link, Text, IconButton } from '@chakra-ui/react';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram } from 'react-icons/fa';
+import { MdOutlineEmail } from 'react-icons/md';
 import RouterLink from '@/components/router-link/RouterLink';
 
 const Footer = () => {
@@ -7,47 +10,43 @@ const Footer = () => {
 			direction="row"
 			padding="32px"
 			justifyContent="space-evenly"
+			alignItems="center"
 			backgroundColor="black"
-			height="30vh"
 		>
-			<Flex direction="column">
+			<Text textStyle="xs">
+				&copy; pressd. made by a music fan. music data from Spotify API.
+			</Text>
+			<Flex direction="row" gap="8px">
 				<RouterLink variant={undefined} color="red" slug="/about">
-					About
+					<Text textStyle="xs">About</Text>
 				</RouterLink>
 				<RouterLink variant={undefined} color="red" slug="/features">
-					Features
+					<Text textStyle="xs">Features</Text>
 				</RouterLink>
 				<RouterLink variant={undefined} color="red" slug="/privacy">
-					Privacy Policy
+					<Text textStyle="xs">Privacy Policy</Text>
 				</RouterLink>
 				<RouterLink variant={undefined} color="red" slug="/terms">
-					Terms of Service
+					<Text textStyle="xs">Terms of Service</Text>
 				</RouterLink>
 			</Flex>
 
-			<Flex direction="column">
-				<Link colorPalette="red" href="https://twitter.com/pressd">
-					Twitter
+			<Flex direction="row" gap="8px">
+				<Link href="https://twitter.com/pressd">
+					<IconButton size="2xs" colorPalette="red" variant="ghost">
+						<FaXTwitter />
+					</IconButton>
 				</Link>
-				<Link colorPalette="red" href="https://instagram.com/pressd">
-					Instagram
+				<Link href="https://instagram.com/pressd">
+					<IconButton size="2xs" colorPalette="red" variant="ghost">
+						<FaInstagram />
+					</IconButton>
 				</Link>
-				<Link colorPalette="red" href="mailto:hello@pressd.app">
-					Contact
+				<Link href="mailto:hello@pressd.app">
+					<IconButton size="2xs" colorPalette="red" variant="ghost">
+						<MdOutlineEmail />
+					</IconButton>
 				</Link>
-			</Flex>
-
-			<Flex direction="column">
-				<Field.Root>
-					<Field.Label>Join our newsletter!</Field.Label>
-					<Group attached w="full" maxW="sm">
-						<Input
-							variant="subtle"
-							placeholder="Enter your email"
-						/>
-						<Button variant="solid">Submit</Button>
-					</Group>
-				</Field.Root>
 			</Flex>
 		</Flex>
 	);
